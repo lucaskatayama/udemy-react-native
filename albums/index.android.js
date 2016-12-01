08afe6cb34f5e4c1cd5762811b87e30c2aa9fa41
Lucas Katayama <lucaskatayama@gmail.com>
2016-12-01 10:02:08 -0200

    fix typo

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,14 +1,16 @@
 // Index.android.js - place code for Android.
 
-
 // Import a library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
 
 // Create a Component
 const App = () => (
-  <Text>Some Text</Text>
+  <Text>
+    Some Text
+  </Text>
+
 );
 
 // Render it to device
-AppRegistry.registerComponent('albuns', () => App);
+AppRegistry.registerComponent('albums', () => App);
