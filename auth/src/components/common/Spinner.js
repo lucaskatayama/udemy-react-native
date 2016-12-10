8b1711c93b2e76ee92eeae4b9e5ddb6b5aee3b97
Lucas Katayama <lucaskatayama@gmail.com>
2016-12-10 20:34:09 -0300

    Adding spinner

---
+++ auth/src/components/common/Spinner.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
+
+const Spinner = ({ size }) => (
+  <View style={styles.spinnerStyle}>
+    <ActivityIndicator size={size || 'large'}/>
+  </View>
+);
+
+
+Spinner.propTypes = {
+  size: PropTypes.string,
+}
+
+export default Spinner;
